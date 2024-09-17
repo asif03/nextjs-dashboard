10e78a4cc552d24102ebfa47aba8ed91f487eece
asif03 <babu033045@gmail.com>
2024-09-17 15:42:21 +0600

    Update Code

--- actions/customerActions.ts
+++ actions/customerActions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { z } from "zod";
+import { date, z } from "zod";
 
 const formSchema = z.object({
   id: z.string(),
@@ -9,14 +9,35 @@ const formSchema = z.object({
     .string()
     .min(2, { message: "Name must be at least 2 characters long." })
     .trim(),
+    avatar: z.
   date: z.string(),
 });
 
 export type State = {
   errors?: {
-    customerId?: number[];
-    amount?: string[];
-    status?: string[];
+    email?: string[];
+    name?: string[];
+    imageUrl?: string[];
   };
   message?: string | null;
+};
+
+const CreateCustomer = formSchema.omit({ id: true, date: true });
+//const UpdateInvoice = formSchema.omit({ date: true, id: true });
+
+export const createCustomer = async (prevState: State, formData: FormData) => {
+  // Validate form fields using Zod
+  const validatedFields = CreateCustomer.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    imageUrl: formData.get("avatar"),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Customer.",
+    };
+  }
 };
--- components/customers/customer-form.tsx
+++ components/customers/customer-form.tsx
@@ -10,32 +10,75 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { useForm } from "react-hook-form";
+import { createCustomer, State } from "@/actions/customerActions";
+import { Button } from "../ui/button";
 
 const CustomerForm = () => {
   const form = useForm();
-  //const [state, formAction] = useFormState(createInvoice, initialState);
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useFormState(createCustomer, initialState);
   return (
-    <Form {...form}>
-      <form className="space-y-8">
-        <div className="rounded-md bg-gray-50 p-4 md:p-6">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input id="name" name={field.name} placeholder="Name" />
-                </FormControl>
-                {state?.errors?.name && (
-                  <FormMessage>{state.errors.name}</FormMessage>
-                )}
-              </FormItem>
-            )}
-          />
-        </div>
-      </form>
-    </Form>
+    <>
+      <Form {...form}>
+        <form action={formAction} className="space-y-8">
+          <div className="rounded-md bg-gray-50 p-4 md:p-6">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input id="name" name={field.name} placeholder="Name" />
+                  </FormControl>
+                  {state?.errors?.name && (
+                    <FormMessage>{state.errors.name}</FormMessage>
+                  )}
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="email"
+                      name={field.name}
+                      placeholder="example@gmail.com"
+                    />
+                  </FormControl>
+                  {state?.errors?.email && (
+                    <FormMessage>{state.errors.email}</FormMessage>
+                  )}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="avatar"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Image</FormLabel>
+                  <FormControl>
+                    <Input id="avatar" type="file" name={field.name} />
+                  </FormControl>
+                  {state?.errors?.email && (
+                    <FormMessage>{state.errors.email}</FormMessage>
+                  )}
+                </FormItem>
+              )}
+            />
+            <div className="pt-2">
+              <Button type="submit">Save</Button>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </>
   );
 };
 
