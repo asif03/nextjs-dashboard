048d05429e7142ee8c71d1fae145996f7ead485d
Asif <babu033045@gmail.com>
2024-09-19 00:04:58 +0600

    Update Code

--- actions/customerActions.ts
+++ actions/customerActions.ts
@@ -3,6 +3,8 @@
 import prisma from "@/lib/db";
 import { z } from "zod";
 import fs from "node:fs/promises";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 3; // 3MB
 const ACCEPTED_FILE_TYPES = ["image/png", "image/jpg", "image/jpeg"];
@@ -54,15 +56,37 @@ export const createCustomer = async (prevState: State, formData: FormData) => {
   }
 
   const orgFileName = validatedFields.data.avatar.name;
+  var fileExt = orgFileName.substring(orgFileName.lastIndexOf(".") + 1);
 
   const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-  const filename = `${orgFileName.replace(/\.[^/.]+$/, "")}-${uniqueSuffix}}`;
+  const filename = `${orgFileName.replace(/\.[^/.]+$/, "")}-${uniqueSuffix}`;
 
   const arrayBuffer = await validatedFields.data.avatar.arrayBuffer();
   const buffer = new Uint8Array(arrayBuffer);
-  await fs.writeFile(`./public/uploads/${filename}`, buffer);
+  await fs.writeFile(`./public/uploads/${filename}.${fileExt}`, buffer);
+  const filePath = `./public/uploads/${filename}.${fileExt}`;
 
-  //console.log(validatedFields.data);
-  // Mutate data
   const { name, email, avatar } = validatedFields.data;
+
+  // Insert data into the database
+  try {
+    await prisma.customers.create({
+      data: {
+        name: name,
+        email: email,
+        image_url: filePath,
+      },
+    });
+    /*return {
+      message: "Invoice created successfully.",
+    };*/
+  } catch (error) {
+    // If a database error occurs, return a more specific error.
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
+
+  revalidatePath("/dashboard/customers");
+  redirect("/dashboard/customers");
 };
